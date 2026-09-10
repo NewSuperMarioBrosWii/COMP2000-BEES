75e35fc441c5597c98e8b1e447afc5aad330316c
Farah Abbas <farah.abbas@students.mq.edu.au>
2026-09-10 16:41:06 +1000

    Added exception handling for empty flower and a generic for random picking and used that generic in pickrandomflower

--- src/Hive.java
+++ src/Hive.java
@@ -30,12 +30,36 @@ public class Hive extends Location {
         Queens.add(myQueen);
     }
 
-    public Flower pickFlower(){
-        int randflower = new Random().nextInt(0, field.flowerfield.size());
-        if(field.flowerfield.get(randflower).occupied == true){
-            pickFlower();
+    public Flower pickFlower(){ // implementing the generic
+        boolean anyFree = false;
+        for (Flower flower: field.flowerfield){
+            if (!flower.occupied){
+                anyFree = true;
+                break;
+
+            }
+            
         }
-        return field.flowerfield.get(randflower);
+        if(!anyFree){
+            throw new NoAvailableFlowerException("No unoccupied flowers in the field");
+        }
+       
+        Flower f = pickRandom(field.flowerfield);
+       if(f.occupied){
+        return pickFlower();
+       }
+       return f;
+    }
+    
+    
+    public class NoAvailableFlowerException extends RuntimeException{ //prevents crash
+        public NoAvailableFlowerException(String message){
+            super(message);
+        }
+    }
+
+    public static <T> T pickRandom(ArrayList<T> list){ //use this generic for any object to pick randomly
+        return list.get(new Random().nextInt(list.size()));
     }
 
     public void DrawHive(){
